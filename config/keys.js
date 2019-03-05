6c114f3c691e72da591b9ef5271a81c304ad4303
Muhammad bilal yousuf <sp14bs0018@maju.edu.pk>
2019-03-05 22:33:39 +0500

    initial

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: 'mongodb://bilal:bilal123@ds349175.mlab.com:49175/thecountrytour-traveler-rating',
+  pusherAppId: '728580',
+  pusherKey: '4c262d023c9ceb6067f3',
+  pusherSecret: 'e1c482153f62cd0837d7',
+  pusherCluster: 'ap2',
+  pusherEncrypted: true
+};
